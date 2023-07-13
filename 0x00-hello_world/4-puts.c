054c7256f51fb892ecb87689969f2f981e7eb9ad
dillin7 <easyarthurloans@gmail.com>
2023-07-13 09:29:57 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 /*
- * main - Entry point
+ * main - Entry point of the program
+ *
+ * This function is the entry point for the program. It prints the message
+ * "Programming is like building a multilingual puzzle" to the console.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle,");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
