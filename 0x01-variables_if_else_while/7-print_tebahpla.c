acae8a26b1c93f5ccb7377fa4d31b567f6cb8eec
dillin7 <easyarthurloans@gmail.com>
2023-07-16 09:17:13 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: get last digit
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 122;
+
+	while (n >= 97)
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n');
+	return (0);
+}
