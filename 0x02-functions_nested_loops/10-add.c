50d2b8fc36b87fa5ad64ad4b026e77e5c1392313
dillin7 <easyarthurloans@gmail.com>
2023-07-18 15:59:35 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *add - add two integers and return the result
+ *
+ *@x: number being added
+ *
+ *@y: number being added
+ *
+ *Return: Always 0.
+*/
+int add(int x, int y)
+{
+	return (x + y);
+}
